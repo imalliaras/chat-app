158600fd34317fda3a070a40d865f15858d31a1a
Gmall <imalliaras@gmail.com>
2018-11-03 12:26:23 +0300

    Added chat form

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,25 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('newMEssage', message);
+    const li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#chat').append(li);
+});
+
+socket.emit('createMessage', {
+    from: 'Frank',
+    text: 'hi'
+}, function(data) {
+    console.log('Got it', data);
+});
+
+$('#chat-form').on('submit', function(event) {
+    event.preventDefault();
+    console.log(event);
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function(data) {
+        // console.log('Got it', data);
+    });
 });
